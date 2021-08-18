580675eff64dad830ae76e98e59d20f59bd81056
JamesLinz <jlalencar@gmail.com>
2021-08-18 16:49:09 -0300

    Programada função transação

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,12 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.mysql.cj.xdevapi.PreparableStatement;
 
 import db.DB;
+import db.DbException;
 import db.DbIntegrityException;
 
 public class Program {
@@ -14,23 +16,38 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
 		try {
 			conn = DB.getConnection();
 			
-			st = conn.prepareStatement(
-					"DELETE FROM seller "
-					+ "WHERE "
-					+ "Id = ?");
+			conn.setAutoCommit(false);
 			
-			st.setInt(1, 8);
+			st = conn.createStatement();
 			
-			int rowsAffected = st.executeUpdate();
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 			
-			System.out.println("Pronto! Linhas afetadas: " + rowsAffected);
+			//int x = 1;
+			//if (x < 2) {
+			//	throw new SQLException("Fake Error");
+			//}
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			conn.commit();
+			
+			System.out.println("rows1 " + rows1);
+			System.out.println("rows2 " + rows2);
+			
+			
 		}
 		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
+			try {
+				conn.rollback();
+				throw new DbException("Transação não ocorreu ! Motivo: " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Erro ao tentar voltar a transação ! Motivo: " + e1.getMessage());
+				
+			}
 		}
 		finally {
 			DB.closeStatement(st);
